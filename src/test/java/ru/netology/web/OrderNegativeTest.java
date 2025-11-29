22e2df1e53f2da821bf0142d3e93089393c2927f
snezhka003 <you@example.com>
2025-11-29 20:50:04 +0300

    added headless in negative test

--- src/test/java/ru/netology/web/OrderNegativeTest.java
+++ src/test/java/ru/netology/web/OrderNegativeTest.java
@@ -25,7 +25,7 @@ public class OrderNegativeTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
